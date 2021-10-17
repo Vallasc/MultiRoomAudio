4427abb265b47af38ac8a3908e0137f000e751f3
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2021-10-17 16:26:38 +0200

    Add js binding interface

--- pc/src/main/Main.java
+++ pc/src/main/Main.java
@@ -8,6 +8,6 @@ public class Main {
     public static void main(String[] args) {
 
         CefApp.startup(args);
-        new UI("MultiRoomAudio");
+        new UI("MultiRoomAudio", JavascriptBindings.class);
     }
 }
