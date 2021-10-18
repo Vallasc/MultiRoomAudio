ae3dd8f27e7ca7bacc7fa5261c59207a03663510
Prop4et <francostanco97@gmail.com>
2021-10-18 17:35:09 +0200

    msgTypes added and some uml charts to decide a couple of things

---
+++ pc/src/message_handler/MsgTypes.java
@@ -0,0 +1,43 @@
+package message_handler;
+
+/**
+ * 
+ * @author Francesco Biancucci
+ * @file MsgTypes.java
+ * @brief defines the types of message exchanged between the different
+ * entities in the system
+ *
+ */
+public class MsgTypes {
+	//position registering
+	public static int REF_POINT = 00; //[client] sends the different signals strength at a reference point during the offline phase
+	public static int SPEAKER_HERE = 01; //[client] same as above but with the poisition of a speaker
+
+	//position handling 
+	public static int SEND_DBM = 10; //[client] same as 00 but during the online phase
+	public static int SELECT_SPEAKER = 11; //[SLAC] message sent to a speaker to let it know it's the one selected
+
+	//music handling request
+	public static int REQ_MUSIC_LIST = 20; //[client] request the music list from the SLAC
+	public static int REQ_MUSIC = 21;//[client] request to play a song to the SLAC
+
+	/*NOTE:
+		qua sono indeciso in realtà, perchè volendo si potrebbe mettere in comunicazione diretta il client con lo speaker per le operazioni di 
+		pausa ecc, solo che poi nel momento in cui cambio stanza lo slac deve prendere lo stato e trasportarlo in un'altra, se lo stato invece
+		è mantenuto direttamente sullo SLAC allora il problema non si pone. 
+		Comunque faccio i diagrammini tra poco per sta roba.
+		*/
+
+	public int PAUSE = 22;//[client] music handling, everything sent to the SLAC
+	public int STOP = 23;//[client]
+	public int RESUME = 24;//[client]
+
+	//music handling response
+	public static int SEND_MUSIC_LIST = 30;//[SLAC] answer to msg 20
+	public static int SEND_MUSIC = 31;//[SLAC] sends a song to the speakers
+
+	//error messages 100-999
+
+
+	
+}
