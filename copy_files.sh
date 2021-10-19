10514b565b7f0701f99b7530988c0a30f5ed1acf
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2021-10-19 21:35:16 +0200

    Update

--- copy_files.sh
+++ copy_files.sh
@@ -1,2 +1,2 @@
 cp -r ./pc/src/it/unibo/sca/multiroomaudio/shared ./android/app/src/main/java/it/unibo/sca/multiroomaudio
-sed -i 's/main.ui.JavascriptInterface/android.webkit.JavascriptInterface/g' ./android/app/src/main/java/it/unibo/sca/multiroomaudio/shared/JavascriptBindings.java
+sed -i 's/it.unibo.sca.multiroomaudio.ui.JavascriptInterface/android.webkit.JavascriptInterface/g' ./android/app/src/main/java/it/unibo/sca/multiroomaudio/shared/JavascriptBindings.java
