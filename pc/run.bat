ad307deb990a904e2ac1ef5303390099ddae50b5
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2021-10-21 12:07:52 +0200

    Update Jcef lib

--- pc/run.bat
+++ pc/run.bat
@@ -1,3 +1,8 @@
-javac -d out -cp "./lib;./lib/*" ./src/it/unibo/sca/multiroomaudio/*.java ./src/it/unibo/sca/multiroomaudio/*/*.java
+javac -d out -cp "./lib;./lib/*" ./src/it/unibo/sca/multiroomaudio/*.java ^
+./src/it/unibo/sca/multiroomaudio/ui/*.java ^
+./src/it/unibo/sca/multiroomaudio/server/*.java ^
+./src/it/unibo/sca/multiroomaudio/shared/*.java ^
+./src/it/unibo/sca/multiroomaudio/shared/messages/*.java ^
+./src/it/unibo/sca/multiroomaudio/shared/dto/*.java
 copy .\lib\*.jar .\out
 java -cp "./out;./out/*;" -Djava.library.path=./lib/win64 it.unibo.sca.multiroomaudio.Main
