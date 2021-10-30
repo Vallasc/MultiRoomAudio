6496a2eeb6672ae4f906a0a65dbcab8f2e3a695c
Prop4et <francostanco97@gmail.com>
2021-10-30 09:42:22 +0200

    scrpti to run the client

--- pc/runclient.sh
+++ pc/runclient.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf outClient
 mkdir outClient
-javac -d outClient ./src/it/unibo/sca/multiroomaudio/ClientMain.java ./src/it/unibo/sca/multiroomaudio/shared/messages/*.java ./src/it/unibo/sca/multiroomaudio/shared/dto/*.java 
+javac -d outClient ./src/it/unibo/sca/multiroomaudio/ClientMain.java ./src/it/unibo/sca/multiroomaudio/shared/messages/*.java ./src/it/unibo/sca/multiroomaudio/shared/dto/*.java ./src/it/unibo/sca/multiroomaudio/shared/IPFinder.java
 
 java -cp "./outClient:./outClient/*" it.unibo.sca.multiroomaudio.ClientMain "$@"
