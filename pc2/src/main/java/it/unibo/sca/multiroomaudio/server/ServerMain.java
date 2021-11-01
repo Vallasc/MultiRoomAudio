23bee3b9de6fa4c6a94e66a1c33573c822ab8715
Vallasc <giacomovallorani.97@gmail.com>
2021-11-01 11:14:54 +0100

    Add http server

--- pc2/src/main/java/it/unibo/sca/multiroomaudio/server/ServerMain.java
+++ pc2/src/main/java/it/unibo/sca/multiroomaudio/server/ServerMain.java
@@ -3,12 +3,23 @@ package it.unibo.sca.multiroomaudio.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import it.unibo.sca.multiroomaudio.server.*;
 
 public class ServerMain {
 
     private final static int servport = 8497;
     public static void main(String[] args){
+
+        // Music http server
+        if(args.length == 2) {
+            (new HttpServer(8080, args[1])).start();
+        } else {
+            (new HttpServer(8080, "C:\\Users\\giaco\\Music")).start(); //TODO
+        }
+
+        // WebApp http server
+        (new HttpServer(80)).start();
+
+
         Thread tcpHandler = new ClientExecutor();
         Thread udpHandler = new DatagramThread();
         
