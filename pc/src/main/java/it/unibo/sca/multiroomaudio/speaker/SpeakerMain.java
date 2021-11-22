b836efae55f4a897987333d6754b5edcb52188b5
Vallasc <giacomovallrani.97@gmail.com>
2021-11-22 19:21:40 +0100

    Add implementation

--- pc/src/main/java/it/unibo/sca/multiroomaudio/speaker/SpeakerMain.java
+++ pc/src/main/java/it/unibo/sca/multiroomaudio/speaker/SpeakerMain.java
@@ -1,5 +1,29 @@
 package it.unibo.sca.multiroomaudio.speaker;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import it.unibo.sca.multiroomaudio.discovery.DiscoveryService;
+
 public class SpeakerMain {
     
+    public static void main(String[] args) {
+        
+        DiscoveryService discovered = new DiscoveryService();
+
+        if (Desktop.isDesktopSupported()){
+            try {
+                Desktop.getDesktop().browse(
+                    new URI("http://"+discovered.getServerAddress().getHostAddress()+":"+discovered.getServerPort()+"/?type=speaker"));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Open " + "http://"+discovered.getServerAddress().getHostAddress()+":"+discovered.getServerPort()+"/?type=speaker");
+        }
+        
+    }
+
 }
