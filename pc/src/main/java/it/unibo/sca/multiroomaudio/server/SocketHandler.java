98b59e81971691f417094c391dbae7d101123728
Vallasc <giacomovallrani.97@gmail.com>
2021-11-23 22:09:48 +0100

    Do not work

--- pc/src/main/java/it/unibo/sca/multiroomaudio/server/SocketHandler.java
+++ pc/src/main/java/it/unibo/sca/multiroomaudio/server/SocketHandler.java
@@ -39,11 +39,11 @@ public class SocketHandler extends Thread{
             MsgHello hello = gson.fromJson(json, MsgHello.class);
             clientId = hello.getId();
             
-            if(dbm.connectedDevices.containsKey(clientId)){//already connected
+            if(dbm.isConnected(clientId)){//already connected
                 dOut.writeUTF(gson.toJson(new MsgHelloBack("REJECTED")));
                 return;
             }
-            dbm.connectedDevices.put(clientId, true);
+            dbm.connectedDevicesSocket.putIfAbsent(clientId, true);
             if(dbm.devices.putIfAbsent(clientId, new Device(hello.getDeviceType(), hello.getMac(), clientId)) != null){
                 dOut.writeUTF(gson.toJson(new MsgHelloBack("home=hello&&type=client", clientId)));
             }else{
