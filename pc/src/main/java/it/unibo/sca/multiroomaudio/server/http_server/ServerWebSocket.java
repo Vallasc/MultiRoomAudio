0d3c99cd32c9336903bf29a633958ac482ef6d0c
Vallasc <giacomovallrani.97@gmail.com>
2021-12-15 22:31:38 +0100

    Minor changes

--- pc/src/main/java/it/unibo/sca/multiroomaudio/server/http_server/ServerWebSocket.java
+++ pc/src/main/java/it/unibo/sca/multiroomaudio/server/http_server/ServerWebSocket.java
@@ -6,9 +6,6 @@ import org.eclipse.jetty.websocket.api.annotations.*;
 import it.unibo.sca.multiroomaudio.server.WebSocketHandler;
 
 import java.io.*;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
-
 
 @WebSocket
 public class ServerWebSocket {
@@ -23,33 +20,19 @@ public class ServerWebSocket {
     public void connected(Session session) {
     }
 
-
     @OnWebSocketMessage
     public void message(Session session, String message) throws IOException {
-        //System.out.println("Got: " + message);   // Print message
         webSocketHandler.handleMessage(session, message);
     }
 
     @OnWebSocketClose
     public void closed(Session session, int statusCode, String reason) {
-        webSocketHandler.handleClose(session);        
-        System.out.println("closed " + statusCode + " " +reason );
-        
+        webSocketHandler.handleClose(session);                
     }
 
     @OnWebSocketError
     public void throwError(Throwable error) {
-        System.out.println(error.getMessage());
+        System.out.println("Web socket error " + error.getMessage() );
     }
-
-    /*synchronized public static void sendAll(Msg message){
-        sessions.forEach((session) -> {
-            try {
-                session.getRemote().sendString( gson.toJson(message) );
-            } catch (IOException e) {
-                System.err.println(e.toString());
-            }
-        });
-    }*/
 
 }
