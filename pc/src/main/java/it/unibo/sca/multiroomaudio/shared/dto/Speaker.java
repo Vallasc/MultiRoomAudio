70522a918825f7287bca1949eb3a2d76e2088277
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2021-12-16 22:16:34 +0100

    Add muted

--- pc/src/main/java/it/unibo/sca/multiroomaudio/shared/dto/Speaker.java
+++ pc/src/main/java/it/unibo/sca/multiroomaudio/shared/dto/Speaker.java
@@ -2,6 +2,7 @@ package it.unibo.sca.multiroomaudio.shared.dto;
 
 public class Speaker extends Device {
     private final String name;
+    private boolean isMuted = false;
 
     public Speaker(String id, String name) {
         super(id);
@@ -10,6 +11,14 @@ public class Speaker extends Device {
 
     public String getName() {
         return name;
+    }
+
+    public void setMuted(boolean isMuted) {
+        this.isMuted = isMuted;
+    }
+
+    public boolean isMuted() {
+        return isMuted;
     }
     
 }
