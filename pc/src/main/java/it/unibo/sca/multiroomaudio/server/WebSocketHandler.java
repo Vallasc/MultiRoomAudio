4d97978080d6265692adf95621d01abb74ba5454
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2021-12-16 22:16:19 +0100

    Hanfle message refactor

--- pc/src/main/java/it/unibo/sca/multiroomaudio/server/WebSocketHandler.java
+++ pc/src/main/java/it/unibo/sca/multiroomaudio/server/WebSocketHandler.java
@@ -8,28 +8,36 @@ import com.google.gson.JsonSyntaxException;
 
 import org.eclipse.jetty.websocket.api.Session;
 
+import it.unibo.sca.multiroomaudio.shared.dto.Client;
+import it.unibo.sca.multiroomaudio.shared.dto.Device;
+import it.unibo.sca.multiroomaudio.shared.dto.Speaker;
 import it.unibo.sca.multiroomaudio.shared.messages.Msg;
 import it.unibo.sca.multiroomaudio.shared.messages.MsgHello;
-import it.unibo.sca.multiroomaudio.shared.messages.player.MsgPause;
 import it.unibo.sca.multiroomaudio.shared.messages.player.MsgPlay;
-import it.unibo.sca.multiroomaudio.shared.messages.player.MsgStop;
 
 public class WebSocketHandler {
     private static final Gson gson = new Gson();
     private final DatabaseManager dbm;  
-    private MusicOrchestrationManager musicManager;
+    private final MusicOrchestrationManager musicManager;
+    private final SpeakerManager speakerManager;
 
 
-    public WebSocketHandler(DatabaseManager dbm, MusicOrchestrationManager musicManager) {
+    public WebSocketHandler(DatabaseManager dbm, MusicOrchestrationManager musicManager, SpeakerManager speakerManager) {
         this.dbm = dbm;
         this.musicManager = musicManager;
+        this.speakerManager = speakerManager;
     }
 
     public void handleClose(Session session){
-        String deviceId = dbm.removeConnectedWebDevice(session);
-        if(deviceId != null){
-            dbm.removeConnectedSocketClient(deviceId); // If it is not a clint does nothing
-            System.out.println("Device [" + deviceId + "] disconnected");
+        Device device = dbm.removeConnectedWebDevice(session);
+        if(device != null){
+            if(device instanceof Client){
+                dbm.removeConnectedSocketClient(device.getId());
+            }
+            if(device instanceof Speaker){
+                speakerManager.updateSpeakerList();
+            }
+            System.out.println("Device [" + device.getId() + "] disconnected");
         }
     }
 
@@ -41,21 +49,29 @@ public class WebSocketHandler {
             MsgHello msg = gson.fromJson(message, MsgHello.class);
             dbm.addConnectedWebDevice(session, msg);
             System.out.println("Device [" + msg.getId() + "] connected");
-        } else if( msgType.equals("PLAY") ){ // Client want to play TODO controllare che sia effettivamente il client
-            MsgPlay msg = gson.fromJson(message, MsgPlay.class);
-            musicManager.playSong(msg.getSongId(), msg.getFromTimeSec());
-        } else if( msgType.equals("PAUSE") ){ // Client want to pause
-            musicManager.pauseCurrentSong();
-        } else if( msgType.equals("STOP") ){ // Client want to stop
-            musicManager.stopCurrentSong();
-        } else if( msgType.equals("NEXT") ){ // Client want next song
-            musicManager.nextSong();
-        } else if( msgType.equals("PREV") ){ // Client want next song
-            musicManager.prevSong();
+            speakerManager.updateSpeakerList();
+        } else{ 
+            Device connected = dbm.getConnectedWebDevice(session);
+            if(connected != null && connected instanceof Client) {
+                if( msgType.equals("PLAY") ){ // Client want to play
+                    MsgPlay msg = gson.fromJson(message, MsgPlay.class);
+                    musicManager.playSong(msg.getSongId(), msg.getFromTimeSec());
+                } else if( msgType.equals("PAUSE") ){ // Client want to pause
+                    musicManager.pauseCurrentSong();
+                } else if( msgType.equals("STOP") ){ // Client want to stop
+                    musicManager.stopCurrentSong();
+                } else if( msgType.equals("NEXT") ){ // Client want next song
+                    musicManager.nextSong();
+                } else if( msgType.equals("PREV") ){ // Client want next song
+                    musicManager.prevSong();
+                }
+            }
         }
     }
 
     public static void sendMessage(Session session, Msg message) throws IOException{
-        session.getRemote().sendString( gson.toJson(message) );
+        synchronized (session) {
+            session.getRemote().sendString( gson.toJson(message) );
+        }
     }
 }
