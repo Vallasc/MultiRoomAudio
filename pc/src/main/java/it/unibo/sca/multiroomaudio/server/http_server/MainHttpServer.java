91c6dd3f63f510ed0eb95603ddfecfd3d3bdb66f
Vallasc <giacomovallorani.97@gmail.com>
2022-01-11 18:48:11 +0100

    Remove REST route

--- pc/src/main/java/it/unibo/sca/multiroomaudio/server/http_server/MainHttpServer.java
+++ pc/src/main/java/it/unibo/sca/multiroomaudio/server/http_server/MainHttpServer.java
@@ -1,9 +1,6 @@
 package it.unibo.sca.multiroomaudio.server.http_server;
 
-import com.google.gson.Gson;
-
 import it.unibo.sca.multiroomaudio.server.DatabaseManager;
-import it.unibo.sca.multiroomaudio.shared.messages.MsgScanRoom;
 
 public class MainHttpServer extends HttpServer {
 
@@ -22,28 +19,5 @@ public class MainHttpServer extends HttpServer {
     }
 
     public void setRoutes(){
-        Gson gson = new Gson();
-        service.path("/offline", () -> {
-            service.put("/start", (req, res) -> {
-                //a start request is coming from a client
-                MsgScanRoom msg = gson.fromJson(req.body(), MsgScanRoom.class);
-                if(dbm.getDeviceStart(msg.getId())) 
-                    return "{\"status\": \"KO\"}";
-                if(dbm.setDeviceStart(msg.getId(), msg.getRoom())){
-                    
-                    return "{\"status\": \"OK\"}";
-                }
-                else
-                    return "{\"status\": \"KO\"}";
-            });
-            service.put("/stop", (req, res) -> {
-                MsgScanRoom msg = gson.fromJson(req.body(), MsgScanRoom.class);
-                if(dbm.setDeviceStop(msg.getId())){
-                    return "{\"status\": \"OK\"}";
-                }
-                else
-                    return "{\"status\": \"KO\"}";
-            });
-        });
     }
 }
