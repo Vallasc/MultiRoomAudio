2b565132ed5f268e12439b834facf835c77ffb71
Vallasc <giacomovallorani.97@gmail.com>
2022-01-11 18:54:51 +0100

    Add deviceId, webSocket

--- ui/src/stores.js
+++ ui/src/stores.js
@@ -1,7 +1,4 @@
 import { writable } from 'svelte/store'
 
-export const text = writable('Testo prova')
-
-window.setText = (t) => {
-    text.set(t);
-}
+export const deviceId = writable('')
+export const webSocket = writable(null)
