48ee63da40fbcc0330b48385fb21894d184ce8b9
Vallasc <giacomovallrani.97@gmail.com>
2022-01-26 17:23:36 +0100

    Remove unnecessary code

--- pc/src/main/java/it/unibo/sca/multiroomaudio/discovery/DiscoveryService.java
+++ pc/src/main/java/it/unibo/sca/multiroomaudio/discovery/DiscoveryService.java
@@ -2,37 +2,141 @@ package it.unibo.sca.multiroomaudio.discovery;
 
 import java.io.IOException;
 import java.net.*;
-import java.util.Enumeration;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import it.unibo.sca.multiroomaudio.shared.messages.MyMsgHandler;
+import it.unibo.sca.multiroomaudio.shared.messages.Msg;
 
 public class DiscoveryService {
     private static final int bufferSize = 1024;
-    String mac;
-    InetAddress broadcast;
-    InetAddress serverAddress;
-    String ip;
-    int serverPort;
-    int fingerprintPort;
-    boolean failed = true;
-    int id;
+
+    private List<InterfaceData> validInterfaces;
+    private int interfaceIndex;
+
+    private InetAddress serverAddress;
+    private int serverPort;
+    private int fingerprintPort;
 
     public DiscoveryService() {
+        validInterfaces = new ArrayList<>();
+        interfaceIndex = 0;
+    }
+
+    private static String buildMac(byte[] mac){
+        StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < mac.length; i++) {
+			sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));		
+		}
+		return sb.toString();
+    }
+
+    private void getSpecs(){
+        try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nInterface : interfaces){
+                if(nInterface.isUp() && !nInterface.isLoopback() && !nInterface.isVirtual()) {
+                    List<InterfaceAddress> addresses = nInterface.getInterfaceAddresses();
+                    for (InterfaceAddress addr : addresses) {
+                        InetAddress inetAddr = addr.getAddress();
+                        InetAddress broadcast = addr.getBroadcast();
+                        // If it has a broadcast Ip then is valid
+                        if (!(inetAddr instanceof Inet6Address) && broadcast != null) {
+                            String mac = buildMac(nInterface.getHardwareAddress());
+                            String ip = addr.getAddress().getHostAddress();
+                            this.validInterfaces.add(new InterfaceData(mac, broadcast, ip));
+                        }
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean discover(){
         getSpecs();
-        discover();
+
+        boolean found = false;
+        DatagramPacket packetReceive = new DatagramPacket(new byte[bufferSize], bufferSize);
+
+        for(interfaceIndex = 0; interfaceIndex < validInterfaces.size(); interfaceIndex++) {
+            InetAddress broadcast = getBroadcast();
+
+            byte[] data = null;
+
+            try { // Craft discovery message
+                data = (new MsgDiscovery()).toByteArray();
+            } catch (IOException e) {
+                System.err.println("Error while crafting the message");
+                continue;
+            }
+
+            DatagramSocket socket = null;
+            try {
+                socket = new DatagramSocket(6263);
+                socket.setSoTimeout(2000);
+            } catch (SocketException e) {
+                System.err.println("Error while creating Datagram socket");
+                continue;
+            }
+
+            try {
+                socket.send(new DatagramPacket(data, data.length, broadcast, 6262));
+                System.out.println("The discovery packet is sent successfully");
+            } catch (IOException e) {
+                socket.close();
+                System.err.println("Error while sending discovery message");
+                continue;
+            }
+
+            try {
+                socket.receive(packetReceive);
+                System.out.println("Discovery ack received");
+            } catch (IOException e) {
+                socket.close();
+                System.err.println("Error receiving discovery ack");
+                continue;
+            }
+
+            socket.close();
+            found = true;
+            break;
+        }
+
+        if( found ) {
+            try {
+                MsgDiscoveredServer discovered = (MsgDiscoveredServer) Msg.fromByteArray(packetReceive.getData());
+                serverAddress = packetReceive.getAddress();
+                fingerprintPort = discovered.getFingerprintPort();
+                serverPort = discovered.getServerPort();
+            }catch (ClassNotFoundException | IOException e){
+                found = false;
+            }
+        }
+
+        return found;
     }
 
     public String getMac(){
-        return mac;
+        if(interfaceIndex < validInterfaces.size())
+            return validInterfaces.get(interfaceIndex).getMac();
+        else
+            return null;
     }
 
     public InetAddress getBroadcast(){
-        return broadcast;
+        if(interfaceIndex < validInterfaces.size())
+            return validInterfaces.get(interfaceIndex).getBroadcast();
+        else
+            return null;
     }
 
-    public boolean getFailed(){
-        return failed;
+    public String getIp(){
+        if(interfaceIndex < validInterfaces.size())
+            return validInterfaces.get(interfaceIndex).getIp();
+        else
+            return null;
     }
 
     public InetAddress getServerAddress(){
@@ -47,122 +151,37 @@ public class DiscoveryService {
         return serverPort;
     }
 
-    public int getId(){
-        return id;
-    }
 
-    public String getIp(){
-        return ip;
-    }
+    private class InterfaceData {
+        private String mac;
+        private InetAddress broadcast;
+        private String ip;
 
-    public static String buildMac(byte[] mac){
-        StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < mac.length; i++) {
-			sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));		
-		}
-		return sb.toString();
-    }
+        private InterfaceData(String mac, InetAddress broadcast, String ip) {
+            this.mac = mac;
+            this.broadcast = broadcast;
+            this.ip = ip;
+        }
 
-    private void getSpecs(){
-        Enumeration<NetworkInterface> ni = null;
-        boolean flagFound = false;
-        try {
-            ni = NetworkInterface.getNetworkInterfaces();
-            while (ni.hasMoreElements() && !flagFound) {
-                NetworkInterface n = ni.nextElement();
-                if(n.isUp() && !n.isLoopback() && !n.isVirtual()){
-                    List<InterfaceAddress> addresses = n.getInterfaceAddresses();
-                    for(InterfaceAddress addr : addresses){
+        public String getMac() {
+            return mac;
+        }
 
-                        InetAddress inetAddr = addr.getAddress();
-                        if (inetAddr instanceof Inet6Address) continue;
-                        Socket socket = new Socket();
-                        socket.setReuseAddress(true);
-                        try {
-                            socket.bind(new InetSocketAddress(inetAddr, 15000));
-                            try{
-                                socket.connect(new InetSocketAddress("google.com", 80), 1000);
-                            }catch(BindException e){
-                                socket.close();
-                                this.mac = buildMac(n.getHardwareAddress());
-                                this.broadcast = addr.getBroadcast();
-                                this.ip=addr.getAddress().getHostAddress();
-                                return;
-                            }
-                            socket.close();
-                            this.mac = buildMac(n.getHardwareAddress());
-                            this.broadcast = addr.getBroadcast();
-                            this.ip=addr.getAddress().getHostAddress();
+        public InetAddress getBroadcast() {
+            return broadcast;
+        }
 
-                          }catch(IOException e) {
-                                System.err.println("socket is still bound");
-                                continue;
-                          }
-                    }
-                }
-                   
-            }
-        } catch (SocketException e) {
-            e.printStackTrace();
+        public String getIp() {
+            return ip;
         }
-    }
-    //private static Couple specs;
-    public void discover(){
-        byte[] data = null;
-        byte[] byteBuffer1 = new byte[bufferSize];  
-        DatagramPacket packetReceive = new DatagramPacket(byteBuffer1, bufferSize);
-        try{
-            data = MyMsgHandler.dtgmOutMsg(new MsgDiscovery());
-        }catch(IOException e){
-            System.err.println("Error while sending the message");
-            return;
+
+        @Override
+        public String toString() {
+            return "InterfaceData{" +
+                    "mac='" + mac + '\'' +
+                    ", broadcast=" + broadcast +
+                    ", ip='" + ip + '\'' +
+                    '}';
         }
-        //---------------------------------------------------------
-        DatagramSocket socket = null;
-        try {
-            socket = new DatagramSocket(6263);
-            socket.setSoTimeout(5000);
-        } catch (SocketException e) {
-            e.printStackTrace();
-            return;
-        }
-        boolean flagResend = true;
-        while(flagResend){
-            try {
-                socket.send(new DatagramPacket(data, data.length, broadcast, 6262));
-            } catch (IOException e) {
-                socket.close();
-                flagResend = false;
-                e.printStackTrace();
-                return;
-            } 
-            System.out.println("The packet is sent successfully");  
-            try {
-                socket.receive(packetReceive);
-                flagResend = false;
-                System.out.println("packets received");
-            } catch (SocketTimeoutException e) {
-                continue;
-            } catch (IOException e) {
-                e.printStackTrace();       
-                socket.close(); 
-                flagResend = false;
-                return;
-            } 
-        }
-        try {
-            Object readObject = MyMsgHandler.dtgmInMsg(packetReceive.getData());
-            if (readObject instanceof MsgDiscoveredServer) 
-                serverAddress = packetReceive.getAddress();  
-            MsgDiscoveredServer discovered = (MsgDiscoveredServer) readObject;
-            fingerprintPort = discovered.getFingerprintPort();
-            serverPort = discovered.getServerPort();
-        }catch (ClassNotFoundException | IOException e){
-            e.printStackTrace();
-            socket.close();
-            return;
-        }
-        socket.close();
-        failed = false;
     }
 }
