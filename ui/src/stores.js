4b70d52abfee5ff1cc6e628ac245817c17ffb8dd
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-02-11 15:37:20 +0100

    Add global variable

--- ui/src/stores.js
+++ ui/src/stores.js
@@ -1,4 +1,7 @@
 import { writable } from 'svelte/store'
 
-export const deviceId = writable('')
+export const deviceId = writable("")
+export const webPort = writable(-1)
+export const musicPort = writable(-1)
+export const hostname = writable("")
 export const webSocket = writable(null)
