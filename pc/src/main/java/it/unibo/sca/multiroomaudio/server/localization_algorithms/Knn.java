cc263c1dada465c63e048388873fdbf999c1fe65
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-03-05 23:51:36 +0100

    Fix knn

--- pc/src/main/java/it/unibo/sca/multiroomaudio/server/localization_algorithms/Knn.java
+++ pc/src/main/java/it/unibo/sca/multiroomaudio/server/localization_algorithms/Knn.java
@@ -1,5 +1,6 @@
 package it.unibo.sca.multiroomaudio.server.localization_algorithms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
@@ -10,6 +11,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
 
 import it.unibo.sca.multiroomaudio.server.DatabaseManager;
 import it.unibo.sca.multiroomaudio.server.FingerprintAnalyzer;
@@ -17,9 +20,11 @@ import it.unibo.sca.multiroomaudio.server.SpeakerManager;
 import it.unibo.sca.multiroomaudio.shared.model.Client;
 import it.unibo.sca.multiroomaudio.shared.model.Room;
 import it.unibo.sca.multiroomaudio.shared.model.ScanResult;
+import it.unibo.sca.multiroomaudio.utils.Utils;
 
 public class Knn extends FingerprintAnalyzer{
-   
+    public static final int POWER_LIMIT = -75;
+
     private HashMap<String, Double> errors = new HashMap<>(); //<AP, error>
     private int k = 1;
     private boolean flagValueError = false;
@@ -64,7 +69,7 @@ public class Knn extends FingerprintAnalyzer{
         }
         return result;
     }
-    private double compute(double x, double mu){
+    private double compute(double x, double mu){ 
         return Math.pow(x-mu, 2);
     }
 
@@ -79,42 +84,37 @@ public class Knn extends FingerprintAnalyzer{
         double[] roomErr = new double[r.getNScan()];
         Arrays.fill(roomErr, 0);
 
-        Arrays.sort(onlines);
-        TreeMap<String, List<ScanResult>> offlines = new TreeMap<>(r.getFingerprints()); 
-        int i = 0; //onlines
-
-        Iterator<String> it = offlines.keySet().iterator();
-        String key = it.next();
-        while(it.hasNext() && i < onlines.length){
-            if(onlines[i].getBSSID().compareTo(key) == 0){//online == offline
-                for(int k = 0; k < offlines.get(key).size(); k++)
-                    roomErr[i] += compute(onlines[i].getSignal(), offlines.get(key).get(k).getSignal());
-                i += 1;
-                key = it.next();
-            }
-            else if(onlines[i].getBSSID().compareTo(key) < 0){//online < offline
-                i += 1;
-            }
-            else if(onlines[i].getBSSID().compareTo(key) > 0){//online > offline
-                if(this.flagValueError){
-                    for(int k = 0; k < offlines.get(key).size(); k++)
-                        roomErr[i] += compute(-100d, offlines.get(key).get(k).getSignal());
+        List<String> notFound = new ArrayList<>(r.getFingerprints().keySet());
+        for(ScanResult online : onlines){
+            notFound.remove(online.getBSSID());
+            // Array scanresult di bssid della stanza
+            ArrayList<ScanResult> offlines = r.getFingerprints(online.getBSSID());
+            if(offlines != null){
+                for(int i = 0; i < offlines.size(); i++){
+                    if(offlines.get(i).getSignal() != Room.SCAN_NOT_FOUND)
+                        roomErr[i] += compute(online.getSignal(), offlines.get(i).getSignal());
                 }
-                key = it.next();
             }
         }
+
 
         if(this.flagValueError){
-            while(it.hasNext()){
-                for(int k = 0; k < offlines.get(key).size(); k++)
-                        roomErr[i] += compute(onlines[i].getSignal(), -100d);
-                    key = it.next();
+            for(String BSSID : notFound){
+                List<ScanResult> offlines = r.getFingerprints(BSSID);
+                for(int i=0; i<offlines.size(); i++){
+                    ScanResult offline = offlines.get(i);
+                    if(offline.getSignal() != Room.SCAN_NOT_FOUND)
+                        roomErr[i] += compute(POWER_LIMIT, offline.getSignal());
+                }
             }
         }
+
 
         for(int j = 0; j < roomErr.length; j++){
             this.errors.put(r.getId()+"_"+j, Math.sqrt(roomErr[j]));
+            //System.out.println(r.getId()+"_"+j + "->" + this.errors.get(r.getId() +"_" + j) );
         }
+        //Utils.sleep(4000);
     }
  
     @Override
@@ -134,13 +134,14 @@ public class Knn extends FingerprintAnalyzer{
         }
         
         this.errors = sortByValueAsc(this.errors);
+
         HashMap<String, Integer> classes = new HashMap<>();
         Iterator<String> it = errors.keySet().iterator();
         int ki = 0;
         while(it.hasNext() && ki < k) {
             String key = it.next().split("_")[0];
             if(classes.containsKey(key)) 
-                classes.put(key, classes.get(key)+1);
+                classes.put(key, classes.get(key) + 1);
             else
                 classes.put(key, 1);
             ki++;
