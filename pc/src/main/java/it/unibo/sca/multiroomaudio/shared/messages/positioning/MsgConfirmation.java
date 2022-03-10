c5525bccbc7b31db6e04fd02f1cc97efcae599b6
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-03-10 09:11:09 +0100

    Add room list

--- pc/src/main/java/it/unibo/sca/multiroomaudio/shared/messages/positioning/MsgConfirmation.java
+++ pc/src/main/java/it/unibo/sca/multiroomaudio/shared/messages/positioning/MsgConfirmation.java
@@ -1,10 +1,18 @@
 package it.unibo.sca.multiroomaudio.shared.messages.positioning;
 
+import java.util.List;
+
 import it.unibo.sca.multiroomaudio.shared.messages.Msg;
 
 public class MsgConfirmation extends Msg{
+    private final List<String> rooms;
 
-    public MsgConfirmation() {
+    public MsgConfirmation(List<String> rooms) {
         super("CONFIRMATION");
+        this.rooms = rooms;
+    }
+
+    public List<String> getRooms() {
+        return rooms;
     }
 }
