202f55b465944d66e28c392755fbd6cb175eca1c
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-03-21 18:21:53 +0100

    Minor changes

--- pc/src/main/java/it/unibo/sca/multiroomaudio/utils/GlobalState.java
+++ pc/src/main/java/it/unibo/sca/multiroomaudio/utils/GlobalState.java
@@ -7,7 +7,7 @@ public class GlobalState {
     private int cutPower = -70;
     private int k = 4;
     private boolean useWeights = false;
-    private boolean confirmRoom = true;
+    private boolean confirmRoom = false;
     private int clientFingerprintWindowSize = 2;
 
     public static GlobalState getInstance() {
